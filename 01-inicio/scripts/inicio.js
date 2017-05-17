188f78be98b25a98aea1a654ae08db56363d635c
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-05-17 21:36:43 +0200

    Js introduccion

--- 01-inicio/scripts/inicio.js
+++ 01-inicio/scripts/inicio.js
@@ -1,3 +1,4 @@
+// JavaScript & EcmaScript ES6
 
 /**
  * Constantes
@@ -9,25 +10,57 @@ const SALUDO ="Hello ";
  * Variables
  */
 
+
+var nEdad = 12;
+var sCantaor1 = "Pepe 'El Lebrijano'"
+var sCantaor2 = 'Juan "El Habichuela"'
+
+var esBueno = true;
+var esMalo = false;
+
+var queSoy;
+queSoy = esMalo;
+
+nEdad = "Pepe"
+
+var sGrupo = `<ul>
+              <li>${sCantaor1}</li>
+              <li>${sCantaor2}</li>
+              <ul>
+         `
+ 
+
+// Operadorres + - * / ^
+
+
+
+sGrupo += `<p> Añade tu otros </p>`
+
 //Acceso al Node btn1 del DOM
 var oBtn1 = document.querySelector("#btn1");
 
 //Acceso al Node nombre del DOM
 var oNombre = document.querySelector("#nombre");
 
+//Acceso al Node  edad del DOM
+var oEdad = document.querySelector('#edad');
+
 //Acceso al Node  respuesta del DOM
 var oRespuesta = document.querySelector("#respuesta");
+
 
 /**
  * Funciones
  */
 
 function saludar(){
-    let sSaludo = SALUDO
+    let sSaludo = SALUDO;
+    let nEdad = oEdad.value;
 
     sSaludo += oNombre.value;
   oRespuesta.innerHTML = "<b>" + sSaludo + "</b>";
-    
+  oRespuesta.innerHTML += sGrupo;
+  oRespuesta.innerHTML += nEdad+2;
 }
 
 oBtn1.onclick= saludar;
@@ -38,3 +71,4 @@ oBtn1.onclick= saludar;
 console.dir(oBtn1);
 console.dir(oNombre);
 console.dir(oRespuesta);
+console.log(nEdad);
