5b6555f414692bd03b9794f66f93a562c7ab7e99
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-05-19 15:59:39 +0200

    ambito

---
+++ 07-ambitos/scripts/inicio.js
@@ -0,0 +1,31 @@
+//Declaracióon de una función
+//function sumar () {}
+
+//Variables global(existe en todas partes)
+var sName= "Funciones"
+
+function main (){
+    sMainName = "Función Main"
+    //let nNum1 = Number(prompt(" Dime tu número",12));
+    //let nNum2 = Number(prompt(" Dime tu otro",23));
+    //let nNum3 = +prompt(" Dime tu número",3);
+    //let nNum4 = +prompt(" Dime tu otro",13);
+    
+    if (2){
+        //let permite dar valores dentro
+        let sName = "Condicional"
+        console.log(sName);
+    }
+
+    console.log(sName);
+    
+
+    
+}
+
+//Invocación,es decir, llamar a la función,decirle que se ejecute
+// Sin los parentesis no hay invocación
+
+main();
+
+console.log(sName);
