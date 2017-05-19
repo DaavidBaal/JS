b64cc1a5a3b89a53827f240a1aeac0d44c6d5eb9
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-05-19 13:11:17 +0200

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,16 @@
+//Ejemplo argumentos
+
+function mayor() {
+    var elMayor = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+        if (arguments[i]>elMayor) {
+            elMayor = arguments [i];
+            
+        }
+        
+    }
+    return elMayor;
+}
+
+var variable1 = mayor(1,3,5,8);
+var variable = mayor(4,6,8,1,2,3,4,5);
