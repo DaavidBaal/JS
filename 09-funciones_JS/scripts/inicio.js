c36251f428395f1c52af658f12e71b82a1fd2cec
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-05-19 17:35:41 +0200

    cambio

--- 09-funciones_JS/scripts/inicio.js
+++ 09-funciones_JS/scripts/inicio.js
@@ -1,44 +1,34 @@
+// Declaracion de una funcion como variable
+// let n_var = "valor variable"
+let sumar = function (a,b) {return +a + +b}
 
+// Declaración de una funcióm arrow (ES6)
 
+let restar = (a,b) => {return a-b}
 
-//Declaracióon de una función como variables
+// Declaracion de una funcion
+function sumarDobles (a, b) {
+    a *= 2
+    b *= 2
+    return a + b;
+}
 
-//let n_var = "valor variable"
-
-
-
-let sumar = function(a,b) { return +a + +b };
-
-//DEclaraciónbn de una función arrow
-
-let restar =  (a,b) => {return a-b}
-
-//Declaración de una función
-
-function sumarDobles(a,b) {
-    a *=2
-    b *=2
-    return  a + b;
+function main () {
+    let nNum1 = 0
+    let nNum2 = 0
+    
+    nNum1 = Number(prompt("Dime un número", 12));
+    nNum2 = Number(prompt("Dime otro", 23));
+    
+    let nResultSumaDobles = sumarDobles (nNum1, nNum2)
+    let nResultSuma = sumar (nNum1, nNum2)
+    let nResultResta = restar (nNum1, nNum2)
+    
+    console.log(nResultSuma);
+    console.log(nResultResta);
+    console.log(nResultSumaDobles);
 }
 
 
-
-function main(){
-    let nNum1 =  0;
-    let nNum2 =  0;
-
-    nNum1 = Number(prompt(" Dime tu número",12));
-    nNum2 = Number(prompt(" Dime tu número",23));
-
-    let nResultSumarDobles = sumarDobles (nNum1, nNum2);
-    let nResultSumar = sumar (nNum1, nNum2);
-    let nResultResta = restar (nNum1, nNum2);
-    
-     console.log(sumarDobles (nResultSumaDobles));
-     console.log(sumar (nResultSuma));
-     console.log(restar (nResultReSta));
-      
-}
-
-    
-main ()
+// Invocación de la funcion
+main()
